d0be1504ec4ef671dd8a816544729753c7cd9d56
Dxm42 <lsantana1234@hotmail.com>
2023-10-10 20:34:55 -0300

    Add: database H2

--- src/main/java/br/com/lucasdxm42/todolist/user/UserController.java
+++ src/main/java/br/com/lucasdxm42/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.lucasdxm42.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 */
 @RestController
 @RequestMapping("/users")
-public class UserController{
+public class UserController {  
+  @Autowired
+  private IUserRepository userRepository;
 /**
  * Tipos de dados
  * Integer (int) numeros inteiros
@@ -25,8 +31,16 @@ public class UserController{
  * Void
  */
 @PostMapping("/")
-public void create(@RequestBody UserModel userModel){
-  System.out.println(userModel.name);
+public ResponseEntity create(@RequestBody UserModel userModel){
+  var user = this.userRepository.findByUsername(userModel.getUsername());
+
+  if(user != null){
+    // Mensagem de erro
+    // Status code
+    return ResponseEntity.status(400).body("Usuário ja existe");
+  }
+  var userCreated = this.userRepository.save(userModel);
+  return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 }
 
 }
